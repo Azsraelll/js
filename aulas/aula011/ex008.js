0d6d3d58a0f0ef59d271dbea5524bd5cff307a75
Azsraelll <rafasardins@gmail.com>
2024-11-22 08:54:26 -0300

    Create ex008.js

---
+++ aulas/aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){
+    console.log("Você Ultrapassou a velocidade permitida, MULTADO")
+}else{
+    console.log("Você está dentro da velocidade permitida")
+}
