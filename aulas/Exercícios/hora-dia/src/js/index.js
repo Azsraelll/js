96508d528fd9ef2e1f2d8e74b5130f961f266394
Azsraelll <rafasardins@gmail.com>
2024-12-02 17:21:54 -0300

    finalizando um exercício

--- aulas/Exercícios/hora-dia/src/js/index.js
+++ aulas/Exercícios/hora-dia/src/js/index.js
@@ -1,4 +1,17 @@
-var hora = document.getElementById('hora')
-hora = new Data()
-hora = hora.getHours()
-document.write(hora.innerHTML)
+function carregar(){
+    var msg = document.getElementById('hora')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()  
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = './src/img/nascer-do-sol.jpg'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >= 12 && hora < 18){
+        img.src = './src/img/por-do-sol.jpg'
+        document.body.style.background = '#b9846f'
+    }else{
+        img.src = './src/img/anoitecer.jpg'
+        document.body.style.background = '#515154'
+    }
+}
